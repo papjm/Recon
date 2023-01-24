f14fb896884be054edda9cf45fc2b8f8780da354
papjm <102404994+papjm@users.noreply.github.com>
2023-01-24 03:21:53 -0500

    Create clone.py
    This line of Code will download all repo's of a github user

---
+++ clone.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+def clone_repos(username):
+    # Make a request to the GitHub API for the given user's repositories
+    url = f'https://api.github.com/users/"username"/repos'
+    repos = requests.get(url).json()
+
+    # Clone each repository
+    for repo in repos:
+        os.system(f'git clone {repo["clone_url"]}')
+
+# Example usage
+clone_repos('username')
